1611f2d9e177172d2c485388ea192aa87dbb82fa
Jacky (Hansjoerg) Baltes <jacky.baltes@gmail.com>
2015-06-27 11:18:46 -0500

    Random selection of sounds.

--- js/main.js
+++ js/main.js
@@ -4,10 +4,23 @@
 
 var resourcesLoading = 0;
 
-sounds = [
-    [ "Media/Black Card.wav", [ [0,1] ], null ],
-    [ "Media/Red Card.wav", [ [0,1] ], null ]
-];
+sounds = {
+    root :{ name: null,
+            clips: [],
+            audio: null,
+            children:{ name: "Media/Introduction.wav", clips: [ { start: 0, end: 0} ], audio: null,
+                children: [
+                    { name: "Media/Black Card.wav",
+                        clips: [ { start: 0, end: 1}, { start: 1, end: 3}, { start: 3, end: 5}, { start: 5, end: 7} ],
+                        audio: null,
+                        children: null }, // Sounds must have full second resolution for start and duration
+                    { name: "Media/Red Card.wav",
+                        clips: [ { start: 0, end: 1}, { start: 1, end: 3}, { start: 3, end: 5}, { start: 5, end: 7} ],
+                        audio: null,
+                        children: null }
+            }
+        }
+};
 
 function loadSound(soundSrc) {
     //alert("Starting to load a sound");
@@ -64,17 +77,99 @@ function loadSound(soundSrc) {
 
 function onResourceLoad() {
     if(resourcesLoading == 0)
-        onLoaded();
+        writeMessage(controller, "The robot will ask you questions about your card.\nYou can say Yes or No,\nyou can tell the truth or lie,\nor you can be quiet.\nJust make sure that you look at the robot." )
+        playGame();
 }
 
-function loadSounds() {
-    var length = sounds.length;
-    for( var i = 0; i < length; i++ ) {
-        console.log("Loading sound " + sounds[i][0]);
-        sounds[i][2] = loadSound(sounds[i][0])
+function loadSounds( node ) {
+    if ( ( node != null ) && ( node.name != null ) ) {
+        node.audio = loadSound( node.name );
+    }
+
+    if ( children != null ) {
+        var length = node.children.length;
+        for (var i = 0; i < length; i++) {
+            loadSounds( node.children[i]);
+        }
     }
 }
 
-function onLoaded() {
-    sounds[0][2].play()
+function playGame() {
+    playRandomSound(sounds.root);
+}
+
+function writeMessage(canvas, message) {
+    var context = canvas.getContext('2d');
+    context.font = '18pt Calibri';
+    context.fillStyle = 'antiquewhite';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = 'black';
+    context.fillText(message, 10, 25);
+}
+
+function getMousePos( canvas, evt ) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+var mousePos = {x: 400, y: 100};
+
+function onDocumentLoaded() {
+    var controller = document.getElementById('controller');
+    console.log("controller " + controller);
+
+    controller.addEventListener('mousemove', function(evt) {
+        mousePos = getMousePos(controller, evt);
+        var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+        writeMessage(controller, message);
+    }, false);
+
+    loadSounds(sounds.root);
+}
+
+var currentSound = null;
+
+function playRandomSound( node ) {
+    var arr = [];
+    var numChildren = 2;
+
+    if ( node.children != null ) {
+        numChildren = node.children.length + 2;
+    }
+
+    var bwidth = controller.width / numChildren;
+    var index = Math.floor( mousePos.x / bwidth )
+
+    for( var i = 0; i < node.children.length; i++ ) {
+        if ( (sounds[i].state >= state + min ) && ( sounds[i].state < state + max ) ) {
+            console.log("Found sound file " + sounds[i].name );
+            for( var j = 0; j < sounds[i].clips.length; j++ ) {
+                if ( sounds[i].clips[j].end != 0 ) {
+                    length = ( sounds[i].clips[j].end - sounds[i].clips[j].start ) * 1000;
+                }  else {
+                    length = ( sounds[i].audio.duration - sounds[i].clips[j].start ) * 1000;
+                }
+                arr.push( { audio: sounds[i].audio, start: sounds[i].clips[j].start, timeout: length } )
+            }
+        }
+    }
+
+
+
+    console.log("Found the following number of options " + arr.length )
+    if ( currentSound != null ) {
+        currentSound.audio.pause();
+    }
+
+    currentSound = arr[Math.floor(Math.random() * arr.length)];
+    if ( currentSound != null ) {
+        console.log("Playing this sound " + currentSound.audio.src)
+
+        currentSound.audio.currentTime = currentSound.start;
+        currentSound.audio.play();
+        setTimeout(playRandomSound(), currentSound.duration);
+    }
 }
